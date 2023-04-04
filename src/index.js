22818a000456a4c705720970abf7ae063a8d7e01
evil-coconut <richard.morgan2951@yandex.ru>
2023-04-04 09:35:01 +0700

    Js for burger

--- src/index.js
+++ src/index.js
@@ -1,2 +1,10 @@
+let icon = document.getElementById('icon-nav');
+let navIcon = document.getElementsByClassName('nav__icon');
+let navBody = document.getElementsByClassName('nav__body');
+
+icon.onclick = function() {
+    navIcon[0].classList.toggle("_active");
+    navBody[0].classList.toggle("_active");
+}
 
 
